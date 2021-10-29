fedb9b402aa431b9dd1d6e999275053d8591361b
Colin Poon <me@colinpoon.com>
2021-10-28 21:47:02 -0700

    axios and selectors

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,5 +1,6 @@
 
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
 import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "./Appointment";
@@ -45,26 +46,32 @@ const appointments = [
 ];
 
 
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
+// const days = [
+//   {
+//     id: 1,
+//     name: "Monday",
+//     spots: 2,
+//   },
+//   {
+//     id: 2,
+//     name: "Tuesday",
+//     spots: 5,
+//   },
+//   {
+//     id: 3,
+//     name: "Wednesday",
+//     spots: 0,
+//   },
+// ];
 
 function Application(props) {
   const [day, setDay] = useState('Monday');
+  const [days, setDays] = useState([]);
+  useEffect(() => {
+    axios.get('/api/days').then(res =>{
+      setDays([... res.data]);
+    });
+}, [])
 
   const appointmentList = appointments.map((appointment) => {
     return (
