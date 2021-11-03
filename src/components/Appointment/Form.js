2a1d30fd5244998c9ab6f78bfbbd065a358b185f
Colin Poon <me@colinpoon.com>
2021-11-03 13:48:15 -0700

    form test for student name on save

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -3,6 +3,13 @@ import InterviewerList from 'components/InterviewerList'
 import Button from 'components/Button'
 
 function Form(props) {
+  
+  // const { interviewers, onCancel, onSave } = props;
+
+  const [student, setStudent] = useState(props.student || "");
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
+  const [error, setError] = useState("");
+
   function reset(){
     setStudent("");
     setInterviewer(null);
@@ -11,8 +18,15 @@ function Form(props) {
     reset();
     props.onCancel();
   }
-  const [student, setStudent] = useState(props.student || "");
-  const [interviewer, setInterviewer] = useState(props.interviewer || null);
+  
+  function validate() {
+    if (student === "") {
+      setError("Student name cannot be blank");
+      return;
+    }
+  
+    props.onSave(student, interviewer);
+  }
 
   return (
     <main className="appointment__card appointment__card--create">
@@ -26,7 +40,9 @@ function Form(props) {
             placeholder="Enter Student Name"
             value={student}
             onChange={(event) => setStudent(event.target.value)}
+            data-testid="student-name-input"
           />
+          { error && <section className="appointment__validation">{error}</section>}
         </form>
         <InterviewerList
           interviewers={props.interviewers}
@@ -37,7 +53,8 @@ function Form(props) {
       <section className="appointment__card-right">
         <section className="appointment__actions">
           <Button danger onClick={cancel}>Cancel</Button>
-          <Button confirm onClick={() => props.onSave(student, interviewer)}>Save</Button>
+          {/* <Button confirm onClick={() => props.onSave(student, interviewer)}>Save</Button> */}
+          <Button confirm onClick={validate}>Save</Button>
         </section>
       </section>
     </main>
--- src/components/__tests__/Appointment.test.js
+++ src/components/__tests__/Appointment.test.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { render, cleanup, fireEvent } from "@testing-library/react";
-import Application from "components/Application";
+// import Application from "components/Application";
+import Appointment from "components/Appointment/index.js";
 
-afterEach(cleanup);
+// afterEach(cleanup);
 
 describe("Appointment", () => {
   it("renders without crashing", () => {
