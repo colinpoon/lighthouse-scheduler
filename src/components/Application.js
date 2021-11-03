189dbac7e4757195d853b8a2fae4fddc6bff25b8
Colin Poon <me@colinpoon.com>
2021-11-02 18:20:19 -0700

    commented out unused imports

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,5 +1,6 @@
-import React, { useState, useEffect } from "react";
-import axios from 'axios';
+import React from "react";
+// import React, { useState, useEffect } from "react";
+// import axios from 'axios';
 
 import "components/Application.scss";
 import DayList from "./DayList";
